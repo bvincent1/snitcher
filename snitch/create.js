a5af7449e485d25de27f1fffd84e23e67bf53eab
bvincent1 <benjc.vincent@gmail.com>
2017-03-02 14:49:47 -0700

    Fix dat stamp, remove excess roles

--- snitch/create.js
+++ snitch/create.js
@@ -6,22 +6,15 @@ const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-depe
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.create = (event, context, callback) => {
-  const timestamp = new Date().getTime();
+  const timestamp = Date.now();
   const data = JSON.parse(event.body);
-  if (typeof data.text !== 'string') {
-    console.error('Validation Failed');
-    callback(new Error('Couldn\'t create the todo item.'));
-    return;
-  }
 
   const params = {
     TableName: process.env.DYNAMODB_TABLE,
     Item: {
       id: uuid.v1(),
-      text: data.text,
-      checked: false,
-      createdAt: timestamp,
-      updatedAt: timestamp,
+      view: data,
+      createdAt: timestamp
     },
   };
 
